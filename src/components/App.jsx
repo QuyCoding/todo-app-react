235d3a6cc647f1916c0c34ceccafb89112bb7b42
quyhoangs <quyromutc@gmail.com>
2024-06-30 17:09:36 +0700

    1.Sau khi todo đã được thêm vào danh sách     1.1 Khi click vào checkbox thì hiển thị hoàn thành gạch nội dung todo     1.2 Khi click 2 lần vào todo đã được thêm vào danh sách         - Cho phép chỉnh sửa todo         - Nhấn enter hoặc click ra ngoài sẽ hoàn thành chỉnh sửa và commit todo mới đánh dầu hoàn thành chỉnh sửa         - Nhấn nút x sẽ hủy chỉnh sửa và giữ nguyên todo cũ     1.3 Khi click vào nút xóa thì xóa todo khỏi danh sách

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,22 @@ import '../reset.css';
 import '../App.css';
 /*
   #TODO 
+  Mô tả tổng quan về các chức năng cần triển khai 
     1. Xử lý hiển thị giá trị mặc định vào todo list khi bấm enter
     2. XỬ lý hiển thị lại giá trị khi input thay đổi vào todo list
+    3. Xử lý xoá todo khi click vào button X
+    4. Xử lý check todo khi click vào checkbox
+       Xử lý đánh dấu chỉnh sửa lại todo khi double click vào todo đã được thêm vào danh sách 
+  #Debug trong input onDoubleClick={() => console.log('Editting')}   
+  
+  #NOTE
+  • autoFocus : tự động focus vào input khi được render
+    - Khi component chứa đoạn mã này được render ra lần đầu tiên,
+     input sẽ tự động có focus và người dùng có thể bắt đầu nhập liệu ngay lập tức mà không cần phải click vào input đó.
+  • onDoubleClick : sự kiện xảy ra khi double click vào một phần tử
+  • onBlur : sự kiện xảy ra khi một phần tử mất focus
+    - Khi người dùng click ra ngoài input, input sẽ mất focus và chuyển về trạng thái ban đầu
+    
 */
 
 function App() {
@@ -13,16 +27,19 @@ function App() {
       id: 1,
       title: 'Đây là Function component react',
       isComplete: false,
+      isEditing: false,
     },
     {
       id: 2,
       title: 'Go Grocery',
       isComplete: true,
+      isEditing: false,
     },
     {
       id: 3,
       title: 'Take over world',
       isComplete: false,
+      isEditing: false,
     },
   ]);
 
@@ -77,6 +94,75 @@ function App() {
     deleteTodo(id);
   };
   
+  //Check todo khi click vào checkbox 
+  /*
+  //Duyệt qua mảng todos, nếu id của todo trùng với id truyền vào thì đảo ngược giá trị isComplete ban đầu
+     NOTE : cách viết này : return {...todo, isComplete: !todo.isComplete}
+      • ...todo : giữ nguyên các giá trị của todo cũ 
+      • isComplete: !todo.isComplete : đảo ngược giá trị isComplete ban đầu trong todo đó
+    => Kết quả trả về là một mảng mới với giá trị isComplete của todo đó đã được đảo ngược
+  */
+  function completeTodo (id){
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, isComplete: !todo.isComplete}
+      }
+      return todo;
+    })
+    setTodos(updatedTodos);
+  }
+  
+  //Mark as editting
+  function markAsEditting(id){
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, isEditing: !todo.isEditing}
+      }
+      return todo;
+    })
+    setTodos(updatedTodos);
+  }
+  
+  //Update todo
+  /*
+    Mô tả : 
+    1. Duyệt qua mảng todos, nếu id của todo trùng với id truyền vào thì cập nhật lại title của todo đó
+    2. Nếu input rỗng thì không cho phép update
+    3. Khi người dùng click ra ngoài input, input sẽ mất focus và chuyển về trạng thái ban đầu
+  */
+  function updateTodo(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        if (event.target.value.trim().length === 0) {
+          todo.isEditing = false;
+          return todo;
+        }
+        todo.title = event.target.value;
+        todo.isEditing = false;
+      }
+      return todo;
+    });
+  
+    setTodos(updatedTodos);
+  }
+  
+  //cancelEdit
+  /* 
+    Mô tả :
+    Trong khi đang chỉnh sửa todo, nếu người dùng nhấn phím Escape thì hủy bỏ việc chỉnh sửa
+    1. Duyệt qua mảng todos, nếu id của todo trùng với id truyền vào thì hủy bỏ việc chỉnh sửa quay lại trạng thái ban đầu
+  */
+ 
+  function cancelEdit(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = false;
+      }
+      return todo;
+    });
+  
+    setTodos(updatedTodos);
+  }
  
   
   return (
@@ -97,9 +183,39 @@ function App() {
           {todos.map((todo, index) => (
             <li key={todo.id} className="todo-item-container">
               <div className="todo-item">
-                <input type="checkbox" />
-                <span className="todo-item-label">{todo.title}</span>
-                {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
+                <input 
+                  type="checkbox" 
+                  checked={todo.isComplete}
+                  onChange={() => completeTodo(todo.id)}
+                />
+              {todo.isEditing ? (
+                <input 
+                type="text" 
+                className="todo-item-input" 
+                // value={todo.title} // Giá trị của input
+                defaultValue={todo.title} //Giá trị mặc định của input có thể nhập liệu lại
+                autoFocus //Tự động focus vào input khi được render
+                onBlur={(event) => updateTodo(event,todo.id)} //Khi người dùng click ra ngoài input, input sẽ mất focus và chuyển về trạng thái ban đầu
+                onKeyDown={(event) => {
+                  if(event.key === 'Enter'){
+                    updateTodo(event,todo.id)
+                  } else if(event.key === 'Escape'){
+                    cancelEdit(event,todo.id)
+                  }
+                }
+              }
+                />
+              ):(
+                <>
+                  <span
+                    onDoubleClick={() => markAsEditting(todo.id)} 
+                    className={`todo-item-label ${todo.isComplete ? 'line-through' : '' }`}
+                  >
+                  {todo.title}
+                  </span>
+                </>
+              )}
+                
               </div>
               <button onClick={handleDelete(todo.id)} className="x-button">
                 <svg
